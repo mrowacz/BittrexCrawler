5352c46320b59e9997df0f654b68ec2612113e11
Łukasz Czerwiński <mrowacz@gmail.com>
2017-11-06 23:47:39 +0100

    crawler: save current working state
    Signed-off-by: Łukasz Czerwiński <mrowacz@gmail.com>

--- BittrexCrawler.py
+++ BittrexCrawler.py
@@ -1,3 +1,4 @@
+import re
 import sys
 import json
 import requests
@@ -38,7 +39,7 @@ class BittrexCrawler(object):
             mongo_markets = set()
             for market in markets.find():
                 mongo_markets.add(market["market"])
-                print (market)
+                logging.info(market)
 
             markets_data  = seq(js['result'])\
                 .filter(lambda x: x['BaseCurrency'] == 'BTC' or x['BaseCurrency'] == 'ETH')\
@@ -63,26 +64,28 @@ class BittrexCrawler(object):
             if js['success'] == True:
                 return js['result']
         else:
-            logging.error('Error during parsing ' + data + " ticker")
+            logging.error('Error during parsing ' + resp.text + " ticker")
 
     def fetch_market(self, name=None):
         url="https://bittrex.com/api/v1.1/public/getticker?market=" + name
-        db = self.client['data']
-        cursor = db[name]
 
         r = requests.get(url)
         data = self.analyze_ticker(r)
+
+        if data is None:
+            return ""
+
         cursor_data = {}
         cursor_data["date"] =  datetime.datetime.now()
         cursor_data["ask"] = data["Ask"]
         cursor_data["bid"] = data["Bid"]
         cursor_data["last"] = data["Last"]
 
-        cursor.insert(cursor_data)
         return data
 
+    # get all data for selected market - just read data from mongodb
     def list_market(self, name):
-        db = self.client['data']
+        db = self.client['bittrex']
         cursor = db[name]
 
         result = []
@@ -91,8 +94,66 @@ class BittrexCrawler(object):
 
         return result
 
+    def add_record(self, name=None, price=None):
+        
+
+    # main function which is the heart of this script
+    def fetch_btc_market(self):
+        # first download and update market list
+        self.get_markets()
+
+        # get market list
+        market_list = []
+        db = self.client['config']
+        markets = db['markets']
+
+        market_list = seq(markets.find())\
+            .map(lambda obj : obj["market"])\
+            .filter(lambda str: re.match("^BTC-", str))
+
+        logging.debug("MARKET LIST=" + str(market_list))
+
+        # get all currencies values
+        db_hist = self.client['history']
+        cursor = db_hist["hist"]
+
+        for market in market_list:
+            logging.info("Fetching - " + market)
+
+            last_price_curr = cursor.find({"market": market}).limit(1)
+            curr_price = self.fetch_market(market)
+
+            if curr_price == '':
+                logging.error("Cannot get data for market=" + markets)
+                continue
+
+            last_price = None
+            for obj in last_price_curr:
+                last_price = obj
+
+            if last_price is None:
+                post = {
+                    "market": market,
+                    "Bid": curr_price["Bid"],
+                    "Ask": curr_price["Ask"],
+                    "Last": curr_price["Last"]
+                }
+                db_hist["hist"].insert(post)
+                logging.info("Creating new last record for " + market + " -- " + str(post))
+            else:
+                logging.error("Not Empty " + str(last_price))
+
+
+            # print last_price, curr_price
+            break
+        # print self.fetch_market(market_list[0])
+
+
+
+
 if __name__ == "__main__":
-    c = BittrexCrawler()
-    #c.get_markets()
-    c.fetch_market("BTC-ETH")
-    c.list_market("BTC-ETH")
+    c = BittrexCrawler(debug_level=logging.DEBUG)
+    # c.get_markets()
+    # print c.fetch_market("BTC-ETH")
+    # print c.list_market("BTC-ETH")
+    c.fetch_btc_market()
